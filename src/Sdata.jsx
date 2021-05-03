7a789950426d07466703c1f2c53db99d8bccf0fe
Prasamsha Sharma <prasamshari@gmail.com>
2021-05-03 13:36:10 -0700

    added link to portfolio projects

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,21 +1,21 @@
-import IMG2 from "./img/slothy.gif";
 import IMG3 from "./img/crystal1.jpg";
-import IMG4 from "./img/minion1.png";
+import IMG4 from "./img/minion2.jpg";
 import IMG6 from "./img/disney.jpg";
 import IMG7 from "./img/giphy.gif";
 import IMG8 from "./img/unpic.jpg";
 import IMG9 from "./img/food.jpg";
 import IMG10 from "./img/fun.jpg";
+import IMG11 from "./img/clicky.jpg";
 
 const Sdata = [
   {
     id: 1,
-    imgsrc: IMG2,
-    title: "Giftastic Animals",
+    imgsrc: IMG11,
+    title: "Clicky Game",
     summary:
-      "This app uses GIPHY API to make a dynamic web page that populates gifs of user choice. When the user clicks on a button, the page grabs 10 static, non-animated gif images from the GIPHY API.",
-    github: "https://github.com/sharmap1/Giftastic",
-    link: "https://sharmap1.github.io/Giftastic/",
+      "Clicky Game is a simple memory game.It is a web app that utilizes React for the front end. Click to score but don't click the same card twice!",
+    github: "https://github.com/sharmap1/Clicky-game",
+    link: "https://polar-sea-94378.herokuapp.com/",
   },
   {
     id: 2,
@@ -63,29 +63,21 @@ const Sdata = [
   },
   {
     id: 7,
-    imgsrc: IMG10,
-    title: "Fun App",
-    summary: "This is a app which will showcase the top 10 Disney movies ever.",
-    github: "https://github.com/sharmap1/Disney-Movies",
-    link: "https://disney-movies.netlify.app/",
-  },
-  {
-    id: 8,
     imgsrc: IMG9,
     title: "React-Recipe App",
     summary:
-      "This is a front end app that uses GIPHY API to make a dynamic web page that populates gifs of user choice.",
-    github: "https://github.com/sharmap1/Giphy-React-Hooks",
-    link: "https://giphytastic.netlify.app/",
+      "This is a front end app that uses Edemamme API to make a dynamic web page that populates recipe of user choice.",
+    github: "https://github.com/sharmap1/Recipe-App",
+    link: "https://festive-ardinghelli-bf981c.netlify.app/",
   },
   {
-    id: 9,
-    imgsrc: IMG8,
-    title: "Unsplash-React App",
+    id: 8,
+    imgsrc: IMG10,
+    title: "Fun App",
     summary:
-      "This is a front end app that uses Unsplash API to make a dynamic web page that populates free images.",
-    github: "https://github.com/sharmap1/Unsplash-React-Hooks",
-    link: "https://freepicsunsplash.netlify.app/",
+      "This is a app which uses APIs to show cat facts, giphy, jokes, trivia questions, pokemon, quotes and unsplash images.",
+    github: "https://github.com/sharmap1/Fun-App-React-Api",
+    link: "https://gallant-hermann-01804b.netlify.app/",
   },
 ];
 
